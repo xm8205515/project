dec3089ebb65753e67a860eb3f2927ead6410252
xm8205515 <309997885@qq.com>
2016-01-19 14:07:33 +0800

    Update tets.java

--- tesst/src/tets.java
+++ tesst/src/tets.java
@@ -12,7 +12,7 @@ public class tets {
 	public static void main(String[] args) {
 //		DrinkWaterServerImplService ds=new DrinkWaterServerImplService();
 //		DrinkWaterIface df=ds.getDrinkWaterServerImplPort();sss
-//		System.out.println(df.findDrinkWater("", "", ""));
+//		System.out.println(df.findDrinkWater("", "", ""));再试试
 		RadiationStationListImplService rmImplService=new RadiationStationListImplService();
 		RadiationStationListIface ri=rmImplService.getRadiationStationListImplPort();
 		System.out.println(ri.getRadiationStationList("", "", "", ""));
