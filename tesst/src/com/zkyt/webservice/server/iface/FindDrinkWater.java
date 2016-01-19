358997dc781e38a2702062ab75996ac8ed440179
xm8205515 <309997885@qq.com>
2016-01-19 13:44:20 +0800

    测试3

--- tesst/src/com/zkyt/webservice/server/iface/FindDrinkWater.java
+++ tesst/src/com/zkyt/webservice/server/iface/FindDrinkWater.java
@@ -1,5 +1,5 @@
 package com.zkyt.webservice.server.iface;
-
+ 
 import javax.xml.bind.annotation.XmlAccessType;
 import javax.xml.bind.annotation.XmlAccessorType;
 import javax.xml.bind.annotation.XmlType;
